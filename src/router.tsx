11ed7b5db83b7bb0dcaaf28c2db4b0ed82a765f3
Nicolás Barreto <nbarreto@acu.com.uy>
2024-12-13 16:08:27 -0300

    Finish

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,13 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexPage from "./views/IndexPage"
-import FavoritesPage from "./views/FavoritesPage"
 import Layout from "./layouts/Layout"
+
+//Lazy loading, para que cargue los componentes segun se necesiten, si hago build con todos los imports se me crea un unico archivo 
+  //y eso hace que la carga inicial sea pesada y que tenga informacion que el usuario no usará y el lazy me lo evita
+const FavoritesPage = lazy(()=> import('./views/FavoritesPage')) 
+
+
 
 export default function AppRouter() {
   return (
@@ -9,7 +15,11 @@ export default function AppRouter() {
         <Routes>
           <Route element={<Layout/>}> 
             <Route path='/' element={<IndexPage />} index/>
-            <Route path='/favoritos' element={<FavoritesPage />}/>
+            <Route path='/favoritos' element={ //Parte del lazy loading, fallback puede tomar incluso otro componente, y es lo que se mostrará mientras carga el componente Favorite
+              <Suspense fallback='Cargando'> 
+                <FavoritesPage/>
+              </Suspense>
+            }/>
           </Route>
         </Routes>
     
